b64fb5cfe641095ee13985b192f0d92e7ff299a0
Luis Guilherme Nascimento Maruccio <luis.maruccio@gmail.com>
2022-08-09 23:29:29 -0300

    Ajust Errors

--- app/helpers/containerHelper.py
+++ app/helpers/containerHelper.py
@@ -54,6 +54,6 @@ class ContainerHelper():
         portsList = dict()
 
         for port in ports:
-            portsList[f'{port.containerport}/{port.protocol}'] = port.value
+            portsList[f'{port.containerport}/{port.protocol}'] = port.externalport
 
         return portsList
